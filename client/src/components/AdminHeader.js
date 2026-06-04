9ef448473b90b49768f50edf78af6a5b45127c4a
PrabeshRimal16 <rimalprabesh002@gmail.com>
2026-06-04 13:10:41 +0545

    feat: update build script to disable CI for production builds and import admin animations styles

--- client/src/components/AdminHeader.js
+++ client/src/components/AdminHeader.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import '../styles/admin-animations.css';
+
 import { Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
