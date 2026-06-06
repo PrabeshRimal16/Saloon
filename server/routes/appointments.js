14978fea848689cf720a38e35f3e6fc965c69911
PrabeshRimal16 <rimalprabesh002@gmail.com>
2026-06-06 23:57:58 +0545

    fix: Improved code in appointments.js

--- server/routes/appointments.js
+++ server/routes/appointments.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../db");
 
+
 // Ensure cancelled_by column exists (safe to run on startup)
 (async () => {
   try {
